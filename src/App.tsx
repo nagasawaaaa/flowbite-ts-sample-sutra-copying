68e9d1f5338469bb698f50847fc27fe8f37bb93f
Satoshi Nagasawa <nagasawa@ham.works>
2023-05-19 15:01:02 +0900

    add: husky and prettier eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,28 @@
 import React from 'react';
 import './App.css';
-import { Alert } from "flowbite-react";
-import { HiInformationCircle } from "react-icons/hi";
+import { Alert } from 'flowbite-react';
+import { HiInformationCircle } from 'react-icons/hi';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="text-4xl font-bold mb-6">Learn Flowbite React</h1>
-      <div className="flex flex-col gap-4">
-        <Alert color="info">
-          <>
-            Lorem ipsum dolor sit amet.
-          </>
-        </Alert>
-        <Alert
-          color="failure"
-          icon={HiInformationCircle}
-        >
-          <>
-            Lorem ipsum dolor sit amet, consectetur.
-          </>
-        </Alert>
-        <Alert
-          color="success"
-          onDismiss={() => alert("Alert dismissed!")}
-        >
-          <>
-            Lorem ipsum dolor sit amet, consectetur adipisicing.
-          </>
-                </Alert>
-      </div>
+const App = (): React.ReactNode => (
+  <div className="App">
+    <h1 className="text-4xl font-bold mb-6">Learn Flowbite React</h1>
+    <div className="flex flex-col gap-4">
+      <Alert color="info">
+        <>Lorem ipsum dolor sit amet.</>
+      </Alert>
+      <Alert color="failure" icon={HiInformationCircle}>
+        <>Lorem ipsum dolor sit amet, consectetur.</>
+      </Alert>
+      <Alert
+        color="success"
+        onDismiss={() => {
+          alert('Alert dismissed!');
+        }}
+      >
+        <>Lorem ipsum dolor sit amet, consectetur adipisicing.</>
+      </Alert>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
